57616fbea224c5a4d50f4b0fbd71151adcf94d46
NorthKuria <kuri4martin@gmail.com>
2022-09-08 17:32:51 +0300

    add seed data and get routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # GET ROUTES
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Welcome to GUMZO" }.to_json
   end
 
+  get '/users' do
+    all_users = User.all
+    all_users.to_json()
+  end
+
+  get '/messages' do
+    all_messages = Message.all
+    all_messages.to_json()
+  end
+
+  # POST ROUTES
+
+  # DELETE ROUTES
+
+  # PATCH ROUTES
 end
